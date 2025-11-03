0b5327b7ab87c470c9cbbd0bede09b35b61c1d91
Ljw <ncc1825@naver.com>
2025-11-03 11:20:57 +0900

    feat/mail: 비동기 처리를 위해 @EnableAsync 추가

--- src/main/java/est/oremi/backend12/bookingfresh/BookingFreshApplication.java
+++ src/main/java/est/oremi/backend12/bookingfresh/BookingFreshApplication.java
@@ -2,8 +2,10 @@ package est.oremi.backend12.bookingfresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BookingFreshApplication {
 
     public static void main(String[] args) {
