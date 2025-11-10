81c5f7686d6d95f93f07a25844504d8507c9c707
BlueStar-jjw <jjwman2@gmail.com>
2025-11-10 17:08:14 +0900

    Feat : Order 관련 3계층 , Dto 구현

---
+++ src/main/java/est/oremi/backend12/bookingfresh/domain/order/OrderRepository.java
@@ -0,0 +1,7 @@
+package est.oremi.backend12.bookingfresh.domain.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order , Long> {
+
+}
