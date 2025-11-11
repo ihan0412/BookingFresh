03ab35ff9535a034b0becab230b2440de807af6a
Ljw <ncc1825@naver.com>
2025-11-11 13:48:15 +0900

    docs(ai): 메서드에 주석 추가

--- src/main/java/est/oremi/backend12/bookingfresh/domain/session/Service/AIMessageService.java
+++ src/main/java/est/oremi/backend12/bookingfresh/domain/session/Service/AIMessageService.java
@@ -42,7 +42,7 @@ public class AIMessageService {
                 .toList();
     }
 
-
+    //유저 메시지, LLM 응답 및 SO 결과물 처리 및 저장
     @Transactional
     public AiMessageResponse handleUserMessage(Consumer user, AiMessageRequest req){
         Session session = sessionRepository.findById(req.getSessionId())
