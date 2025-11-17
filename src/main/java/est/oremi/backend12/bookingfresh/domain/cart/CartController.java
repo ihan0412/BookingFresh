3de34dfcc13f5e4660063cc85bf8045db5fb49f0
BlueStar-jjw <jjwman2@gmail.com>
2025-11-17 13:16:54 +0900

    Test : Cart 페이지 test용 commit

--- src/main/java/est/oremi/backend12/bookingfresh/domain/cart/CartController.java
+++ src/main/java/est/oremi/backend12/bookingfresh/domain/cart/CartController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cart")
+@RequestMapping("/api/cart")
 @RequiredArgsConstructor
 public class CartController {
 
